13551488c843152cfa48e1ed8c7d0d703027eac0
Sean Enck <enckse@gmail.com>
2017-07-22 08:45:22 -0400

    remove unwraps

--- src/lib.rs
+++ src/lib.rs
@@ -7,20 +7,29 @@ use libc::size_t;
 use libc::strncpy;
 
 fn write_string(output: *mut c_char, src: &str, output_size: size_t) {
-    let c_str = CString::new(src).unwrap();
-    unsafe { strncpy(output, c_str.as_ptr(), output_size) };
+    let c_str = CString::new(src);
+    if c_str.is_err() {
+        return
+    } else {
+        unsafe { strncpy(output, c_str.unwrap().as_ptr(), output_size) };
+    }
 }
 
 #[allow(non_snake_case)]
 #[no_mangle]
 #[cfg_attr(all(target_os = "windows", target_arch = "x86"), export_name = "_RVExtension@12")]
 pub extern "C" fn RVExtension(output: *mut c_char, output_size: size_t, function: *const c_char) {
-    let f_str = unsafe { CStr::from_ptr(function).to_str().unwrap() };
+    let function_result = unsafe { CStr::from_ptr(function).to_str() };
     let o_str: String;
-    if f_str == "version" {
-        o_str = String::from("0.0.1");
+    if function_result.is_err() {
+        o_str = String::from("no_function");
     } else {
-        o_str = String::from("done");
+        let f_str = function_result.unwrap();
+        if f_str == "version" {
+            o_str = String::from("0.0.1");
+        } else {
+            o_str = String::from("done");
+        }
     }
 
     write_string(output, &o_str, output_size);
