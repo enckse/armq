0022a45e281ee196e79ab3a4365779937253e3ae
Sean Enck <enckse@gmail.com>
2017-07-22 08:37:27 -0400

    all warnings cleaned up

--- src/lib.rs
+++ src/lib.rs
@@ -3,28 +3,25 @@ extern crate libc;
 use std::ffi::CStr;
 use std::ffi::CString;
 use libc::c_char;
-use libc::c_int;
+use libc::size_t;
 use libc::strncpy;
-use std::io::prelude::*;
-use std::net::TcpStream;
 
-fn write_string(output: *mut c_char, src: &str) {
+fn write_string(output: *mut c_char, src: &str, output_size: size_t) {
     let c_str = CString::new(src).unwrap();
-    unsafe { strncpy(output, c_str.as_ptr(), src.len()) };
+    unsafe { strncpy(output, c_str.as_ptr(), output_size) };
 }
 
+#[allow(non_snake_case)]
 #[no_mangle]
 #[cfg_attr(all(target_os = "windows", target_arch = "x86"), export_name = "_RVExtension@12")]
-pub extern "C" fn RVExtension(output: *mut c_char, output_size: c_int, function: *const c_char) {
-    let f_str = unsafe { CStr::from_ptr(function).to_str() };
+pub extern "C" fn RVExtension(output: *mut c_char, output_size: size_t, function: *const c_char) {
+    let f_str = unsafe { CStr::from_ptr(function).to_str().unwrap() };
     let o_str: String;
     if f_str == "version" {
         o_str = String::from("0.0.1");
     } else {
-        let mut stream = TcpStream::connect("127.0.0.1:34254").unwrap();
-        let _ = stream.write(f_str.as_bytes());
         o_str = String::from("done");
     }
 
-    write_string(output, &o_str);
+    write_string(output, &o_str, output_size);
 }
