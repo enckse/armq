5823f6d1140260267f6e162710a72a1d684a5571
Sean Enck <enckse@gmail.com>
2017-08-03 13:32:22 -0400

    integrations now tested

--- src/integrations/r3.d
+++ src/integrations/r3.d
@@ -57,3 +57,25 @@ public static string handle(string input)
     // NOTE: always 'ok' response
     return format("[\"ok\", %s]", respond);
 }
+
+///
+unittest
+{
+    auto resp = handle("connect");
+    assert("[\"ok\", true]" == resp);
+    resp = handle("separator`test");
+    assert("[\"ok\", \"`\"]" == resp);
+    resp = handle("");
+    assert("" == resp);
+    resp = handle("player");
+    assert("[\"ok\", \"\"]" == resp);
+    resp = handle("event");
+    assert("[\"ok\", \"\"]" == resp);
+    resp = handle("command`test");
+    assert("[\"ok\", \"unknown\"]" == resp);
+    resp = handle("replay`abc`xyz");
+    assert("[\"ok\", \"", resp[0..8]);
+    assert("\"]" == resp[resp.length - 2..resp.length]);
+    import std.conv: to;
+    to!int(resp[9..resp.length - 2]);
+}
